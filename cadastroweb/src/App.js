77c02caecdf81c0f522de58ec96fb9b7ed38bd59
Vitor Oliveira <vitorsaop@gmail.com>
2021-06-10 11:18:52 -0300

    montando estrutura p/ formulário de cadastro

---
+++ cadastroweb/src/App.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Formulario de Cadastro</h1>
+      <FormularioCadastro />
+    </Fragment>
+  );
+}
+
+export default App;
